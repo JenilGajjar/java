1db2cd8461bb22f7318ace94e0b8ae802539e4da
JenilGajjar <93389197+JenilGajjar@users.noreply.github.com>
2022-02-15 11:13:28 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,7 @@
+public class Input{
+	public static void main(String[] args) {
+		System.out.println("Name :"+args[0]);
+		System.out.println("City :"+args[1]);
+		System.out.println("Class:"+args[2]);
+	}
+}
