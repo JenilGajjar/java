076ed628f5327bdff107a29cbd2d1ffa68e569a7
JenilGajjar <93389197+JenilGajjar@users.noreply.github.com>
2022-05-10 22:10:23 +0530

    Delete Area_of_circle.java

--- Area_of_circle.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Area_of_circle{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter radius ");
-		int r = sc.nextInt();
-		System.out.println("Area of circle is "+ (3.14*r*r)); 
-	}
-}
