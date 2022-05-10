044850c20aa6d00771883d869d54de15be7c54f9
JenilGajjar <93389197+JenilGajjar@users.noreply.github.com>
2022-05-10 22:10:06 +0530

    Delete Input.java

--- Input.java
+++
@@ -1,7 +0,0 @@
-public class Input{
-	public static void main(String[] args) {
-		System.out.println("Name :"+args[0]);
-		System.out.println("City :"+args[1]);
-		System.out.println("Class:"+args[2]);
-	}
-}
